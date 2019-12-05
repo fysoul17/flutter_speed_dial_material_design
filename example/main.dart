3a3318ce47a81b4124bd41589e3b16fd2e9484d3
fysoul17 <terry@gixgames.com>
2019-12-05 11:45:51 +1100

    Initial Commit

---
+++ example/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial_material_design/flutter_speed_dial_material_design.dart';
+import 'package:flutter_speed_dial_material_design/layout.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('FAB with Speed dial Sample')),
+      floatingActionButton: _buildFloatingActionButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+      bottomNavigationBar: _buildBottomBar(),
+    );
+  }
+
+  Widget _buildFloatingActionButton() {
+    final icons = [
+      SpeedDialAction(child: Icon(Icons.mode_edit)),
+      SpeedDialAction(child: Icon(Icons.date_range)),
+      SpeedDialAction(child: Icon(Icons.list)),
+    ];
+
+    return SpeedDialFloatingActionButton(
+      actions: icons,
+      childOnFold: Icon(Icons.event_note),
+      childOnUnfold: Icon(Icons.close),
+      useRotateAnimation: true,
+      onAction: _onSpeedDialAction,
+    );
+  }
+
+  _onSpeedDialAction(int selectedActionIndex) {
+    print('$selectedActionIndex Selected');
+  }
+
+  Widget _buildBottomBar() {
+    return BottomAppBar(
+      //shape: ExtendedNotchedShape(),
+      shape: CircularNotchedRectangle(),
+      notchMargin: 4.0,
+      child: Container(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () => {},
+            ),
+            IconButton(
+              icon: Icon(Icons.today),
+              onPressed: () => {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ test/flutter_speed_dial_material_design_test.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_speed_dial_material_design/flutter_speed_dial_material_design.dart';
+
+void main() {}
